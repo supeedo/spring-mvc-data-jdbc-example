aee841b48890cfb067b60a5f906942ae21f48edf
Sergey Yakovlev <web-wind@ya.ru>
2021-10-06 09:29:38 +0300

    add validation field

--- src/main/java/ru/study/servlets/AddEmployeeServlet.java
+++ src/main/java/ru/study/servlets/AddEmployeeServlet.java
@@ -20,6 +20,8 @@ import java.util.List;
 @WebServlet(urlPatterns = {"/add-employees"}, name = "addEmployee")
 public class AddEmployeeServlet extends HttpServlet {
     private static final Logger logger = LoggerFactory.getLogger(AddEmployeeServlet.class);
+
+    private static final String ERROR = "Поле не должно быть пустым, содержать только буквы и быть длиной от 2 до 23 символов";
     private EmployeeService service;
 
 
@@ -30,21 +32,45 @@ public class AddEmployeeServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        final Employee employee = new Employee();
-        employee.setFirstName(req.getParameter("firstName"));
-        employee.setLastName(req.getParameter("lastName"));
-        employee.setRole(req.getParameter("role"));
-        service.addEmp(employee);
-        RequestDispatcher view = req.getRequestDispatcher("/WEB-INF/view/allEmployees.jsp");
-        final List<Employee> employees = service.getAllEmp();
-        req.setAttribute("employees", employees);
-        view.forward(req, resp);
-
+        boolean error = false;
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        String role = req.getParameter("role");
+        if (checkEmployeeField(firstName)) {
+            req.setAttribute("errorFirstName", ERROR);
+            error = true;
+        }
+        if (checkEmployeeField(lastName)) {
+            req.setAttribute("errorLastName", ERROR);
+            error = true;
+        }
+        if(checkEmployeeField(role)){
+            req.setAttribute("errorRole", ERROR);
+            error = true;
+        }
+        if(!error) {
+            final Employee employee = new Employee();
+            employee.setFirstName(firstName);
+            employee.setLastName(lastName);
+            employee.setRole(req.getParameter("role"));
+            service.addEmp(employee);
+            RequestDispatcher view = req.getRequestDispatcher("/WEB-INF/view/allEmployees.jsp");
+            final List<Employee> employees = service.getAllEmp();
+            req.setAttribute("employees", employees);
+            view.forward(req, resp);
+        }else{
+            req.getRequestDispatcher("/WEB-INF/view/addEmployee.jsp").forward(req, resp);
+        }
     }
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         this.service = new EmployeeServiceImpl(new EmployeeRepositoryCSVImpl());
+    }
+
+    boolean checkEmployeeField(String name) {
+        String regex = "^([А-Я]{1}[а-яё]{1,23}|[A-Z]{1}[a-z]{1,23})$";
+        return !name.matches(regex);
     }
 }
--- src/main/webapp/WEB-INF/view/addEmployee.jsp
+++ src/main/webapp/WEB-INF/view/addEmployee.jsp
@@ -1,4 +1,6 @@
 <%@ page pageEncoding="UTF-8" contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
 <head>
     <title>Add new Employee</title>
@@ -6,12 +8,30 @@
 <body>
 <fieldset>
     <form method="POST" action='add-employees' name="addEmployee" >
-        First Name : <input
-            type="text" name="firstName"/> <br/>
-        Last Name : <input
-            type="text" name="lastName" /> <br/>
-        Role : <input
-            type="text" name="role"/> <br/>
+        First Name : <label>
+        <input type="text" name="firstName"/>
+    </label> <br/>
+        <c:set var="errorFN" scope="session" value="${errorFirstName}"/>
+        <c:if test="${errorFN != null}">
+        <p style="color:red;size: 5px"><c:out value="${errorFN}"/><p>
+        </c:if>
+
+        Last Name : <label>
+        <input type="text" name="lastName" />
+    </label> <br/>
+            <c:set var="errorLN" scope="session" value="${errorLastName}"/>
+            <c:if test="${errorFN != null}">
+        <p style="color:red;size: 5px"><c:out value="${errorLN}"/><p>
+        </c:if>
+
+        Role : <label>
+        <input type="text" name="role"/>
+    </label> <br/>
+            <c:set var="errorR" scope="session" value="${errorRole}"/>
+            <c:if test="${errorFN != null}">
+        <p style="color:red;size: 5px"><c:out value="${errorR}"/><p>
+        </c:if>
+
         <input type="submit" value="Submit"/>
     </form>
 </fieldset>
