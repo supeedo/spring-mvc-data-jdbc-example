6e5c65fa4a0d609362e0b4bbaaf89efe80f6eb39
Sergey Yakovlev <web-wind@ya.ru>
2021-10-11 21:42:55 +0300

    refactor validation methods

--- src/main/java/ru/study/validation/EmployeeValidator.java
+++ src/main/java/ru/study/validation/EmployeeValidator.java
@@ -3,7 +3,7 @@ package ru.study.validation;
 import ru.study.model.Employee;
 
 public class EmployeeValidator implements Validator<Employee> {
-    private static final String ERROR = " must not be empty, contains only letters, and be from 2 to 23 characters long";
+    private static final String ERROR_MESSAGE = " must not be empty, contains only letters, and be from 2 to 23 characters long";
     private final String namePattern = "^([А-ЯA-Z]{1}[а-яёa-z]{1,23})$|^([А-ЯA-Z]{1}[а-яёa-z]{1,23})+\\s([А-ЯA-Zа-яёa-z]{1}[а-яёa-z]{1,23})$";
     private final ValidationResult vr;
 
@@ -11,35 +11,32 @@ public class EmployeeValidator implements Validator<Employee> {
         this.vr = new ValidationResult();
     }
 
-    private EmployeeValidator checkFirstName(Employee employee) {
-        if (!employee.getFirstName().matches(namePattern)) {
+    private void checkFirstName(String firstName) {
+        if (!firstName.matches(namePattern)) {
             vr.setValid(false);
-            vr.inputMessage("First Name" + ERROR);
+            vr.inputMessage("First Name" + ERROR_MESSAGE);
         }
-        return this;
     }
 
-    private EmployeeValidator checkLastName(Employee employee) {
-        if (!employee.getLastName().matches(namePattern)) {
+    private void checkLastName(String lastName) {
+        if (!lastName.matches(namePattern)) {
             vr.setValid(false);
-            vr.inputMessage("Last Name" + ERROR);
+            vr.inputMessage("Last Name" + ERROR_MESSAGE);
         }
-        return this;
     }
 
-    private EmployeeValidator checkRole(Employee employee) {
-        if (!employee.getRole().matches(namePattern)) {
+    private void checkRole(String role) {
+        if (!role.matches(namePattern)) {
             vr.setValid(false);
-            vr.inputMessage("Role" + ERROR);
+            vr.inputMessage("Role" + ERROR_MESSAGE);
         }
-        return this;
     }
 
     @Override
     public ValidationResult validate(Employee employee) {
-        checkFirstName(employee)
-                .checkLastName(employee)
-                .checkRole(employee);
+        checkFirstName(employee.getFirstName());
+        checkLastName(employee.getLastName());
+        checkRole(employee.getRole());
         return vr;
     }
 }
