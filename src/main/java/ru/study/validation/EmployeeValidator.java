ce4bc809483802620449681bcd826f9c90bfb3e8
Sergey Yakovlev <web-wind@ya.ru>
2021-10-12 22:55:23 +0300

    изменение валидации по Компоновщику

--- src/main/java/ru/study/validation/EmployeeValidator.java
+++ src/main/java/ru/study/validation/EmployeeValidator.java
@@ -1,42 +1,30 @@
 package ru.study.validation;
 
 import ru.study.model.Employee;
+import ru.study.validation.fieldsValidators.FirstNameEmployeeValidator;
+import ru.study.validation.fieldsValidators.LastNameEmployeeValidator;
+import ru.study.validation.fieldsValidators.RoleEmployeeValidator;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class EmployeeValidator implements Validator<Employee> {
-    private static final String ERROR_MESSAGE = " must not be empty, contains only letters, and be from 2 to 23 characters long";
-    private final String namePattern = "^([А-ЯA-Z]{1}[а-яёa-z]{1,23})$|^([А-ЯA-Z]{1}[а-яёa-z]{1,23})+\\s([А-ЯA-Zа-яёa-z]{1}[а-яёa-z]{1,23})$";
-    private final ValidationResult vr;
+    private final ValidationResult validResult;
+    private final Set<Validator<Employee>> validators;
 
     public EmployeeValidator() {
-        this.vr = new ValidationResult();
-    }
-
-    private void checkFirstName(String firstName) {
-        if (!firstName.matches(namePattern)) {
-            vr.setValid(false);
-            vr.inputMessage("First Name" + ERROR_MESSAGE);
-        }
-    }
-
-    private void checkLastName(String lastName) {
-        if (!lastName.matches(namePattern)) {
-            vr.setValid(false);
-            vr.inputMessage("Last Name" + ERROR_MESSAGE);
-        }
-    }
-
-    private void checkRole(String role) {
-        if (!role.matches(namePattern)) {
-            vr.setValid(false);
-            vr.inputMessage("Role" + ERROR_MESSAGE);
-        }
+        this.validResult = new ValidationResult();
+        this.validators = new HashSet<>();
+        validators.add(new FirstNameEmployeeValidator());
+        validators.add(new LastNameEmployeeValidator());
+        validators.add(new RoleEmployeeValidator());
     }
 
     @Override
     public ValidationResult validate(Employee employee) {
-        checkFirstName(employee.getFirstName());
-        checkLastName(employee.getLastName());
-        checkRole(employee.getRole());
-        return vr;
+        for (Validator<Employee> vl : validators) {
+            validResult.merge(vl.validate(employee));
+        }
+        return validResult;
     }
 }
